4f573cacb6326daedbc77fc97e604ca5e9925356
Tokancz <148338850+Tokancz@users.noreply.github.com>
2026-03-27 10:42:43 +0100

    feat: start of responsivity

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
         <p>{{ user.username }}</p>
       </div>
       <ProgressBar :type="'level'" :length="15"/>
+      <img v-if="mobileLayout" @click="mobileMenuOpened = true" src="/img/icons/Menu.svg" alt="Menu Icon" aria-hidden="true" class="menu-icon">
     </header>
 
     <CurrencyDisplay />
@@ -22,22 +23,13 @@
         <p>Created by Mates</p>
       </footer>
     </main>
-
-    <aside>
-      <nav>
-        <li><router-link to="/Pattern-Factory/patterns">PATTERNS</router-link></li>
-        <li><router-link to="/Pattern-Factory/upgrades">UPGRADES</router-link></li>
-        <li><router-link to="/Pattern-Factory/machines">MACHINES</router-link></li>
-        <li><router-link to="/Pattern-Factory/inventory">INVENTORY</router-link></li>
-        <li><router-link to="/Pattern-Factory/prestige">PRESTIGE</router-link></li>
-      </nav>
-      <img src="/img/Stripes.png" alt="Stripes Background" aria-hidden="true" draggable="false">
-    </aside>
+    <Navbar v-if="!mobileLayout"/>
   </template>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue"
+import { onMounted, computed, ref} from "vue"
+import { useWindowSize } from "@vueuse/core"
 import { useSlotStore } from "@/stores/slot"
 import { useUpgradeStore } from "@/stores/upgrade"
 import { useUserStore } from "@/stores/user"
@@ -49,10 +41,15 @@ import ProgressBar from "@/components/ui/ProgressBar.vue"
 import Simulation from "@/components/game/Simulation.vue"
 import Stats from "./components/game/Stats.vue"
 import Login from "./components/ui/Login.vue"
+import Navbar from "./components/game/Navbar.vue"
 
 const slotStore = useSlotStore()
 const upgradeStore = useUpgradeStore()
 const user = useUserStore()
+
+const { width } = useWindowSize()
+const mobileLayout = computed(() => width.value < 1024)
+const mobileMenuOpened = ref(false)
 
 onMounted(() => {
   const data = loadGame()
@@ -86,6 +83,19 @@ div#app {
   "header currency"
   "main aside";
 
+  @media (width <= 1200px) {
+    font-size: .8em;
+  }
+  @media (width < 1024px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 100px 1fr 1fr 200px;
+    grid-template-areas: 
+    "header"
+    "currency"
+    "main"
+    "aside";
+  }
+
   //overflow: hidden;
 
   header {
@@ -112,6 +122,10 @@ div#app {
         text-decoration: underline;
       }
     }
+    img.menu-icon {
+      width: 48px;
+      user-select: none;
+    }
   }
   main {
     grid-area: main;
@@ -133,59 +147,6 @@ div#app {
       > p {
         font-size: 0.9em;
       }
-    }
-  }
-  aside {
-    //height: calc(100dvh - 100px);
-    grid-area: aside;
-
-    display: grid;
-    grid-template-rows: 6fr 3fr;
-    grid-template-areas: 
-    "nav"
-    "img";
-    overflow: hidden;
-
-    nav {
-      grid-area: nav;
-      @include flexColumn(0, space-around);
-
-      li {
-        width: 100%;
-        height: 100%;
-
-        @include flexRow(0, center, center);
-
-        border-bottom: 2px solid var(--primary);
-        border-top: 2px solid var(--primary);
-        text-align: end;
-
-        padding: 0 20px;
-        cursor: pointer;
-        user-select: none;
-        transition: .3s;
-
-        &:hover {
-          background-color: var(--primary);
-          color: var(--black);
-        }
-        &:hover a {
-          color: var(--black);
-        }
-        a {
-          display: block;
-          width: 100%;
-          color: var(--primary);
-          font-family: "ivy-presto";
-          font-size: 4em;
-          text-decoration: none;
-          list-style: none;
-        }
-      }
-    }
-    img {
-      height: 100%;
-      user-select: none;
     }
   }
 }
---
+++ src/components/game/Navbar.vue
@@ -0,0 +1,72 @@
+<template>
+    <aside>
+        <nav>
+            <li><router-link to="/Pattern-Factory/patterns">PATTERNS</router-link></li>
+            <li><router-link to="/Pattern-Factory/upgrades">UPGRADES</router-link></li>
+            <li><router-link to="/Pattern-Factory/machines">MACHINES</router-link></li>
+            <li><router-link to="/Pattern-Factory/inventory">INVENTORY</router-link></li>
+            <li><router-link to="/Pattern-Factory/prestige">PRESTIGE</router-link></li>
+        </nav>
+      
+        <img src="/img/Stripes.png" alt="Stripes Background" aria-hidden="true" draggable="false">
+    </aside>
+</template>
+
+<style lang="scss">
+  aside {
+    grid-area: aside;
+
+    display: grid;
+    grid-template-rows: 6fr 3fr;
+    grid-template-areas: 
+    "nav"
+    "img";
+    overflow: hidden;
+
+    @media (width <= 1024px) {
+      font-size: .9em;
+    }
+
+    nav {
+      grid-area: nav;
+      @include flexColumn(0, space-around);
+
+      li {
+        width: 100%;
+        height: 100%;
+
+        @include flexRow(0, center, center);
+
+        border-bottom: 2px solid var(--primary);
+        border-top: 2px solid var(--primary);
+        text-align: end;
+
+        padding: 0 20px;
+        cursor: pointer;
+        user-select: none;
+        transition: .3s;
+
+        &:hover {
+          background-color: var(--primary);
+          color: var(--black);
+        }
+        &:hover a {
+          color: var(--black);
+        }
+        a {
+          display: block;
+          width: 100%;
+          color: var(--primary);
+          font-family: "ivy-presto";
+          font-size: 4em;
+          text-decoration: none;
+          list-style: none;
+        }
+      }
+    }
+    img {
+      height: 100%;
+      user-select: none;
+    }
+  }
+</style>
